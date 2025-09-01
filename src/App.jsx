30725027ec84a315a8bc39167bd60e7f74be5d07
mjfy93 <maria.jose.ferro@hotmail.com>
2025-09-01 00:32:49 -0500

    First Draft

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import './App.css'
+import './assets/AllOff.jpg'
+import { useState } from 'react'
 
 function App() {
+
+let click = event =>{
+  console.log(event);
+  const color = event.target.className
+  if (color == 'red') { return 'red.active'
+
+  }
+}
+
+
   return (
     <>
-      <h1>Hello World</h1>
+      <div onClick={click} className='trafficLight'>
+        <div className='red'>red</div>
+        <div className='yellow'>yellow</div>
+        <div className='green'>green</div>
+      </div>
     </>
   )
 }
